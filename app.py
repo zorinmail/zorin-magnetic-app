b36f88bfaa2c666cdc0054c2147b85dfb743a7b1
zorinmail <zorinmail@yandex.ru>
2020-06-24 22:41:08 +0500

    temporary delete plot

--- app.py
+++ app.py
@@ -36,36 +36,6 @@ app.title = ('Indices')
 app.layout = html.Div(
     [
 
-        dcc.Graph(
-            id='basic-interactions',
-            figure={
-                'data': [
-                    {
-                        'x': [1, 2, 3, 4],
-                        'y': [4, 1, 3, 5],
-                        'text': ['a', 'b', 'c', 'd'],
-                        'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
-                        'name': 'Trace 1',
-                        'mode': 'markers',
-                        'marker': {'size': 12}
-                    },
-                    {
-                        'x': [1, 2, 3, 4],
-                        'y': [9, 4, 1, 4],
-                        'text': ['w', 'x', 'y', 'z'],
-                        'customdata': ['c.w', 'c.x', 'c.y', 'c.z'],
-                        'name': 'Trace 2',
-                        'mode': 'markers',
-                        'marker': {'size': 12}
-                    }
-                ],
-                'layout': {
-                    'clickmode': 'event+select'
-                }
-            }
-        ),
-
-
         html.Div([
             html.H1(children = 'Индексы геомагнитной активности', style = {'margin-bottom': '10px'}),
         ], style = {'text-align': 'center'}),

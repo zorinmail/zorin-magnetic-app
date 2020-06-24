38b0462881149938e5e065f43a453d764ac69402
zorinmail <zorinmail@yandex.ru>
2020-06-24 22:38:24 +0500

    change title

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app = dash.Dash(server=server, external_stylesheets=external_stylesheets)
 
 app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})
 
-
+app.title = ('Indices')
 
 app.layout = html.Div(
     [

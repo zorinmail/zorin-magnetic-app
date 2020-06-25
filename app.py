caca5448beab10f5230cf6e7009deb5a461bdd88
zorinmail <zorinmail@yandex.ru>
2020-06-25 23:37:50 +0500

    try bootstrap

--- app.py
+++ app.py
@@ -370,8 +370,8 @@ def update_output(date_begin, date_end, time_begin, time_end, time_step,
                   sought_info_1, sought_info_2, sought_info_3, sought_info_4, sought_info_5, sought_info_6):
     sought_info = sought_info_1 + sought_info_2 + sought_info_3 + sought_info_4 + sought_info_5 + sought_info_6
 
-    if date_begin is None or date_end is None:
-        if time_step is None:
+    if date_begin is not None or date_end is not None:
+        if time_step is not None:
             if sought_info.size() > 0:
                 try:
                     b = model.mainFunction(str(date_begin), str(time_begin)+':00', str(date_end), str(time_end)+':00', str(time_step), sought_info)

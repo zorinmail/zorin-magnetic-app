068bbb81b031053fa08d39c0c893a18f57e3e8e0
zorinmail <zorinmail@yandex.ru>
2020-06-27 23:42:30 +0500

    add class to connect_to_db.py

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,11 +1,41 @@
 import sqlalchemy
 
-def create_engine():
-    pg_user = 'axtwbbjtiajlqn'
-    pg_pass = 'b08a23d4e8da8bb01e89b5c02e29fa7b959a3a91be994ac1e7ec0c20fb322615'
-    pg_host = 'ec2-34-230-231-71.compute-1.amazonaws.com'
-    pg_port = '5432'
-    pg_dbname = 'de51bdao4jre75'
-    connection_string = 'postgresql+psycopg2://' + pg_user + ':' + pg_pass + '@' + pg_host + ':' + pg_port + '/' + pg_dbname
+# класс со всякой исходной информацией для подключения
+class connectionInfo:
+
+    def __init__(self, key):
+        """Constructor"""
+        self._pg_user = ''
+        self._pg_pass = ''
+        self._pg_host = ''
+        self._pg_port = ''
+        self._pg_dbname = ''
+        self.connection_string = ''
+        self.__set_params(key)
+
+    def __set_params(self, key):
+        if key == 'server':
+            self._pg_user = 'axtwbbjtiajlqn'
+            self._pg_pass = 'b08a23d4e8da8bb01e89b5c02e29fa7b959a3a91be994ac1e7ec0c20fb322615'
+            self._pg_host = 'ec2-34-230-231-71.compute-1.amazonaws.com'
+            self._pg_port = '5432'
+            self._pg_dbname = 'de51bdao4jre75'
+        elif key == 'local':
+            self._pg_user = 'postgres'
+            self._pg_pass = 'admin'
+            self._pg_host = 'localhost'
+            self._pg_port = '5432'
+            self._pg_dbname = ''
+        self.connection_string = ('postgresql+psycopg2://' +
+                                   self._pg_user + ':' +
+                                   self._pg_pass + '@' +
+                                   self._pg_host + ':' +
+                                   self._pg_port + '/' +
+                                   self._pg_dbname)
+
+
+def create_engine(key):
+    conInfo = connectionInfo(key)
+    connection_string = conInfo.connection_string
     engine = sqlalchemy.create_engine(connection_string)
     return engine
--- model.py
+++ model.py
@@ -1,21 +1,8 @@
 import pandas as pd
 import psycopg2
-# import sqlalchemy
 
 import connect_to_db
-engine = connect_to_db.create_engine()
-
-# pg_user = 'axtwbbjtiajlqn'
-# pg_pass = 'b08a23d4e8da8bb01e89b5c02e29fa7b959a3a91be994ac1e7ec0c20fb322615'
-# pg_host = 'ec2-34-230-231-71.compute-1.amazonaws.com'
-# pg_port = '5432'
-# pg_dbname = 'de51bdao4jre75'
-#
-# connection_string = 'postgresql+psycopg2://' + pg_user + ':' + pg_pass + '@' + pg_host + ':' + pg_port + '/' + pg_dbname
-#
-# engine = sqlalchemy.create_engine(connection_string)
-
-
+engine = connect_to_db.create_engine('server')
 
 # усреднение по времени
 def time_selection(df, timestep):
@@ -25,7 +12,6 @@ def time_selection(df, timestep):
     dfff = dff.round(1)
     dfff['datetime'] = dff.index
     return dfff
-
 
 # # запрос-заплатка
 # sought_info = ['ae','au','al','ao',
@@ -50,7 +36,7 @@ def create_sql_query(date_begin, time_begin, date_end, time_end, sought_info: li
 
 
 def mainFunction(date_begin, time_begin, date_end, time_end, time_step, sought_info: list):
-    sqlquery= create_sql_query(date_begin, time_begin, date_end, time_end, sought_info)
+    sqlquery = create_sql_query(date_begin, time_begin, date_end, time_end, sought_info)
     df = pd.read_sql_query(sqlquery, con=engine)
     df = time_selection(df, time_step)
     return df

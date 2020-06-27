bfeb427a22bd30d6cce9de71aa57e0b8b444a3ac
zorinmail <zorinmail@yandex.ru>
2020-06-27 14:02:43 +0500

    try to make betetr view

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ app.title = ('Indices')
 
 
 
-#controls = dbc.Card( # !!!!! почему card?
-controls = [
+controls = dbc.Card( # !!!!! почему card?
+    [
         html.Div([
             html.H3('Выберите параметры времени', style={'text-align': 'center'}),
         ]),
@@ -272,8 +272,8 @@ controls = [
                  )]
                  ),
     ],
-    #body=True,
-#)
+    body=True,
+)
 
 
 
